4d30a6ba4d1bc1e366ea83cf3a64e79844790d5e
the1alt <thibaut.faurel@gmail.com>
2016-11-25 16:28:22 +0100

    legere avancée formulaire

--- app/contact/contact.controller.js
+++ app/contact/contact.controller.js
@@ -5,18 +5,20 @@
 
     'use strict';
 
-    angular.module('app').controller('contactCtrl', contactCtrl);
+    angular.module('app').controller('contactCtrl', ['$window',contactCtrl]);
 
-    function contactCtrl() {
+    function contactCtrl($window) {
+
         var vm = this;
         vm.submitForm = submitForm;
         vm.title = "Page d'ajout de personnage";
+        vm.checkSaison=[];
 
         // function to submit the form after all validation has occurred
         function submitForm(isValid) {
             // check to make sure the form is completely valid
             if (isValid) {
-                alert('our form is amazing');
+              $window.alert('our form is amazing');
             }
         }
 
