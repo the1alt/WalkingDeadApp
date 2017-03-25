f46af758795388c9eae1e0cd3cdd493f4a7d93de
the1alt <thibaut.faurel@gmail.com>
2017-03-25 18:49:40 +0100

    try tu deploy

--- app/routes.js
+++ app/routes.js
@@ -9,7 +9,7 @@
      * All Routing here...
      * Each Route can resolve some datas before send in controller
      */
-    var walkingapp = angular.module('walkingapp', ['ngRoute', 'ngAnimate'])
+    angular.module('app', ['ngRoute', 'ngAnimate'])
     .config(function($routeProvider) {
       $routeProvider
             .when('/', {
