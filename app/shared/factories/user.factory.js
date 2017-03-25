d28381e236430117b46fed50d3d79a848353192e
the1alt <thibaut.faurel@gmail.com>
2017-03-25 19:59:08 +0100

    try tu deploy

--- app/shared/factories/user.factory.js
+++ app/shared/factories/user.factory.js
@@ -31,7 +31,7 @@
          */
         function getAll() {
             var deferred = $q.defer();
-            $http.get('../../data/characteres.json')
+            $http.get('http://localhost:3000/data')
                 .success(function(data) {
                     deferred.resolve(data);
                     $log.info('API chargée');
@@ -39,24 +39,25 @@
             return deferred.promise; //return a promise with $q library (not exist in ES5)
         }
 
-        // function addOne(obj) {
-        //             var deferred = $q.defer();
-        //             $http.post('../../data', obj)
-        //                 .success(function (data) {
-        //                     deferred.resolve(data);
-        //                 }).error(deferred.reject);
-        //             return deferred.promise; //return a promise with $q library (not exist in ES5)
-        // }
-        //
-        // function removeOne(id) {
-        //     var deferred = $q.defer();
-        //     console.log(id);
-        //     $http.delete("../../data" + id)
-        //         .success(function (data) {
-        //             deferred.resolve(data);
-        //         }).error(deferred.reject);
-        //     return deferred.promise; //return a promise with $q library (not exist in ES5)
-        // }
+        function addOne(obj) {
+                    var deferred = $q.defer();
+
+                    $http.post('http://localhost:3000/data/', obj)
+                        .success(function (data) {
+                            deferred.resolve(data);
+                        }).error(deferred.reject);
+                    return deferred.promise; //return a promise with $q library (not exist in ES5)
+        }
+
+        function removeOne(id) {
+            var deferred = $q.defer();
+            console.log(id);
+            $http.delete("http://localhost:3000/data/" + id)
+                .success(function (data) {
+                    deferred.resolve(data);
+                }).error(deferred.reject);
+            return deferred.promise; //return a promise with $q library (not exist in ES5)
+        }
 
     }
 
